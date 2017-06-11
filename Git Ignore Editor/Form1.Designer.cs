62d49a621454e5471fdb3b850787622e6a1fac56
James Owen <jamesowen012@gmail.com>
2017-06-11 18:13:46 +1000

    Base Upload

---
+++ Git Ignore Editor/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Git_Ignore_Editor {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.treeView1 = new System.Windows.Forms.TreeView();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.button1 = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// treeView1
+			// 
+			this.treeView1.Location = new System.Drawing.Point(12, 42);
+			this.treeView1.Name = "treeView1";
+			this.treeView1.Size = new System.Drawing.Size(211, 377);
+			this.treeView1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(13, 13);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(94, 18);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "label1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(687, 430);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.treeView1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TreeView treeView1;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ Git Ignore Editor/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using FolderSelect;
+
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace Git_Ignore_Editor {
+
+	public partial class Form1 : Form {
+
+		private FolderSelectDialog m_FolderSelect = new FolderSelectDialog();
+
+		private string m_Dir;
+
+		private struct FileFolderHolder {
+			public string filename;
+			public string path;
+
+			public bool isExcluded;
+
+			public bool isFile;
+
+			public TreeNode node;
+		}
+
+		private string[] m_GitIgnore;
+
+		public Form1() {
+			InitializeComponent();
+			//ofofd.AcceptFiles = false;
+			//ofofd.RootFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+			TreeNode node = treeView1.Nodes.Add("test");
+			node.Nodes.Add("test1");
+			treeView1.Nodes.Add("test2");
+			node.Expand();
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			//folderBrowserDialog1.ShowDialog();
+			bool dr = m_FolderSelect.ShowDialog();
+			if(dr) {
+				treeView1.Nodes.Clear();
+				m_Dir = m_FolderSelect.FileName;
+				label1.Text = m_Dir + "\\";
+
+				m_GitIgnore = File.ReadAllLines(m_Dir + "\\.gitignore");
+
+
+
+				addFolders("", null);
+				addFiles("", null);
+			}
+		}
+
+
+		private void addFolders(string a_Suffix,TreeNode a_Node) {
+			string dir = m_Dir + a_Suffix;
+
+			string[] folders = Directory.GetDirectories(dir);
+
+			for (int i = 0; i < folders.Length; i++) {
+				string[] folderSlit = folders[i].Split('\\');
+				string folderName = folderSlit[folderSlit.Length - 1];
+
+				if(LikeOperator.LikeString(folders[i], "*.git", CompareMethod.Binary)) {
+					continue;
+				}
+				if (!isFileAllowed(a_Suffix + "/" + folderName + "/")) {
+					continue;
+				}
+
+				TreeNode node;
+				if (a_Node == null) {
+					node = treeView1.Nodes.Add(folderName + "\\");
+				} else {
+					node = a_Node.Nodes.Add(folderName + "\\");
+				}
+				string nextSuffix = a_Suffix + "\\" + folderName;
+				addFolders(nextSuffix, node);
+				addFiles(nextSuffix, node);
+			}
+		}
+
+		private void addFiles(string a_Suffix, TreeNode a_Node) {
+			string dir = m_Dir + a_Suffix;
+
+			string[] folders = Directory.GetFiles(dir);
+
+			for (int i = 0; i < folders.Length; i++) {
+				string[] folderSlit = folders[i].Split('\\');
+				string folderName = folderSlit[folderSlit.Length - 1];
+
+				if (LikeOperator.LikeString(folders[i], "*.gitignore", CompareMethod.Binary)) {
+					continue;
+				}
+				if (!isFileAllowed(folders[i])) {
+					continue;
+				}
+
+				if (a_Node == null) {
+					treeView1.Nodes.Add(folderName);
+				} else {
+					a_Node.Nodes.Add(folderName);
+				}
+			}
+		}
+
+
+		private bool isFileAllowed(string a_FileDir) { 
+			for(int i = 0; i < m_GitIgnore.Length; i++) {
+				if (LikeOperator.LikeString(a_FileDir, m_GitIgnore[i] , CompareMethod.Binary)) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+	}
+}
